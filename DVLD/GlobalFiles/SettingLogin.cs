cf87e99eb70c87ca1c7c80999b9bb46e2f764590
Administrator <Administrator@WIN-1LKCROFP55E>
2026-08-03 21:24:11 +0400

    update

--- DVLD/GlobalFiles/SettingLogin.cs
+++ DVLD/GlobalFiles/SettingLogin.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BusinessDVLDLayer;
+using Microsoft.Win32;
 
 namespace DVLD.GlobalClass
 {
@@ -19,32 +20,14 @@ namespace DVLD.GlobalClass
 
             try
             {
-                //this will get the current project directory folder.
-                string currentDirectory = System.IO.Directory.GetCurrentDirectory();
+
+                string KeyPath = @"HKEY_CURRENT_USER\SOFTWARE\DVLD";
+
+                Registry.SetValue(KeyPath, "UserName", Username);
+                Registry.SetValue(KeyPath, "Password", Password);
 
 
-                // Define the path to the text file where you want to save the data
-                string filePath = currentDirectory + "\\data.txt";
-
-                //incase the username is empty, delete the file
-                if (Username == "" && File.Exists(filePath))
-                {
-                    File.Delete(filePath);
-                    return true;
-
-                }
-
-                // concatonate username and passwrod withe seperator.
-                string dataToSave = Username + "#//#" + Password;
-
-                // Create a StreamWriter to write to the file
-                using (StreamWriter writer = new StreamWriter(filePath))
-                {
-                    // Write the data to the file
-                    writer.WriteLine(dataToSave);
-
-                    return true;
-                }
+                return true;
             }
             catch (Exception ex)
             {
@@ -56,38 +39,25 @@ namespace DVLD.GlobalClass
 
         public static bool GetStoredCredential(ref string Username, ref string Password)
         {
-            //this will get the stored username and password and will return true if found and false if not found.
             try
             {
-                //gets the current project's directory
-                string currentDirectory = System.IO.Directory.GetCurrentDirectory();
 
-                // Path for the file that contains the credential.
-                string filePath = currentDirectory + "\\data.txt";
+                string KeyPath = @"HKEY_CURRENT_USER\SOFTWARE\DVLD";
 
-                // Check if the file exists before attempting to read it
-                if (File.Exists(filePath))
+                string _UserName, _Password = "";
+
+                _UserName = Registry.GetValue(KeyPath, "UserName", null) as string;
+                _Password = Registry.GetValue(KeyPath, "Password", null) as string;
+
+                if (_UserName != null && _Password != null)
                 {
-                    // Create a StreamReader to read from the file
-                    using (StreamReader reader = new StreamReader(filePath))
-                    {
-                        // Read data line by line until the end of the file
-                        string line;
-                        while ((line = reader.ReadLine()) != null)
-                        {
-                            Console.WriteLine(line); // Output each line of data to the console
-                            string[] result = line.Split(new string[] { "#//#" }, StringSplitOptions.None);
-
-                            Username = result[0];
-                            Password = result[1];
-                        }
-                        return true;
-                    }
+                    Username = _UserName;
+                    Password = _Password;
+                    return true;
                 }
                 else
-                {
                     return false;
-                }
+
             }
             catch (Exception ex)
             {
